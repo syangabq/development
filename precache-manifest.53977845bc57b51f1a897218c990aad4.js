011d12e7dd1b0bc3299fa6d6a4062487a1060741
Sophie Yang <sophie_yang@brown.edu>
2018-12-06 21:35:41 -0500

    Updates

---
+++ precache-manifest.53977845bc57b51f1a897218c990aad4.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "92a93be8c20635bfa43d69674e4f1b1d",
+    "url": "/development/static/media/harry.92a93be8.jpg"
+  },
+  {
+    "revision": "a3bfc53f6471c5827b07",
+    "url": "/development/static/css/main.acbdaed0.chunk.css"
+  },
+  {
+    "revision": "1669a29bb9244dc2358c",
+    "url": "/development/static/js/1.1669a29b.chunk.js"
+  },
+  {
+    "revision": "65e9689cba3ab22d00d9",
+    "url": "/development/static/js/runtime~main.65e9689c.js"
+  },
+  {
+    "revision": "0fea9b0404582bcc4c630099fbc2506f",
+    "url": "/development/static/media/ginny.0fea9b04.jpg"
+  },
+  {
+    "revision": "6fec0a7a61e22a53a0faa4c2ab910264",
+    "url": "/development/static/media/luna.6fec0a7a.jpg"
+  },
+  {
+    "revision": "24413e58ca1842e8f1e36f3274d71ec7",
+    "url": "/development/static/media/draco.24413e58.jpg"
+  },
+  {
+    "revision": "fe0006905d349391580eec7500e87751",
+    "url": "/development/static/media/vincent.fe000690.jpg"
+  },
+  {
+    "revision": "33cc3fc99e37d8fdd8054a29c9328bc4",
+    "url": "/development/static/media/hermione.33cc3fc9.jpg"
+  },
+  {
+    "revision": "a3bfc53f6471c5827b07",
+    "url": "/development/static/js/main.a3bfc53f.chunk.js"
+  },
+  {
+    "revision": "d6cc0df8719b5533656ff71c8f789a44",
+    "url": "/development/static/media/ron.d6cc0df8.jpg"
+  },
+  {
+    "revision": "e58b420bc30e3b788034cc8e37cb677a",
+    "url": "/development/static/media/padma.e58b420b.jpg"
+  },
+  {
+    "revision": "949e7bcf85ff567363c09c147cb67993",
+    "url": "/development/static/media/ernie.949e7bcf.jpg"
+  },
+  {
+    "revision": "c0b7f58175cd7da9c1dfd359b122eaf4",
+    "url": "/development/static/media/susan.c0b7f581.png"
+  },
+  {
+    "revision": "07ce93d8b25fa859f90b9dc2afdf5a93",
+    "url": "/development/static/media/katie.07ce93d8.jpg"
+  },
+  {
+    "revision": "c3639027babba7d4b8dccc2678967fdb",
+    "url": "/development/static/media/percy.c3639027.jpg"
+  },
+  {
+    "revision": "d7d03e05aa37084c94aa48439cd3f8b6",
+    "url": "/development/static/media/oliver.d7d03e05.jpg"
+  },
+  {
+    "revision": "49f3947540b23a547363d97a48b3438f",
+    "url": "/development/static/media/charlie.49f39475.jpg"
+  },
+  {
+    "revision": "ae716ccc52d706649256303aefb0c70a",
+    "url": "/development/index.html"
+  }
+];
